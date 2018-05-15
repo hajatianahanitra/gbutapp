6036241a2ee89a0cf1f7fa06ca1c0ceae87a5a37
Hajatiana <hajatiana.ramamonjisoa@gmail.com>
2018-05-15 15:18:59 +0300

    commit 15.05
    commit 15.05

---
+++ server.js
@@ -0,0 +1,14 @@
+var express  = require('express');
+var connect = require('connect');
+var bodyParser = require('body-parser');
+var app      = express();
+var port     = process.env.PORT || 8080;
+// Configuration
+app.use(express.static('public'));
+app.use(connect.logger('dev'));
+app.use(connect.json());  
+app.use(connect.urlencoded());
+app.use(connect.bodyParser());
+require('./routes/routes.js')(app);
+app.listen(port);
+console.log('The App runs on port ' + port);
